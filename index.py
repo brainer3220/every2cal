f0a2ec7b64ea996dd0b00e8e5a0c85fc5fe89f59
brainer <brainer@kakao.com>
2021-12-21 21:05:09 +0900

    Reformat index.py

--- index.py
+++ index.py
@@ -1,11 +1,12 @@
 from flask import Flask
-from flask import send_file
-from flask import request
 from flask import render_template
+from flask import request
+from flask import send_file
 
 from every2cal import DownCal
 
 app = Flask(__name__)
+
 
 @app.route("/")
 @app.route("/home")
@@ -23,10 +24,10 @@ def dwn_cal():
     try:
         cal = DownCal(start_date, end_date, user_id, user_password)
         return send_file(cal,
-                        start_date + end_date + 'csv',
-                        as_attachment=True)
+                         start_date + end_date + 'csv',
+                         as_attachment=True)
     except:
-        return'''
+        return '''
         <h1>로그인 정보 혹은 시간표 존재 유무를 다시 확인해주세요.</h1>
         '''
 
@@ -35,6 +36,7 @@ def dwn_cal():
     #                  mimetype='text/csv',
     #                  attachment_filename='downloaded_file_name.csv',# 다운받아지는 파일 이름. 
     #                  as_attachment=True)
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
