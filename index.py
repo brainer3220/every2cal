db2c58846080409ab1e038e3684f0f4a702b9ec9
brainer <brainer@kakao.com>
2022-03-15 17:56:09 +0900

    Debug mode false

--- index.py
+++ index.py
@@ -70,4 +70,4 @@ def sitemap_xml():
     return render_template('sitemap.xml')
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8888, debug=True)
+    app.run(host='0.0.0.0', port=8888, debug=False)
